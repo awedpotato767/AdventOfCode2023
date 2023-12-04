0c2fea1416515b514664008d4d258312b3350cd1
awedpotato767 <github@binkle.co.uk>
2023-12-04 10:10:19 +0000

    solution to part one of day 1 	modified:   Program 	new file:   src/input.txt 	modified:   src/main.c 	new file:   src/test.txt 	new file:   src/test2.txt

--- 1/src/main.c
+++ 1/src/main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
+#define MAX_STRING_LENGTH 128
 
-int main (){
-  printf("hello world\n");
+int main (int argc, char *argv[]){
+  FILE *inputptr;
+  inputptr = fopen(argv[1],"r");
+  if(inputptr == NULL){
+    printf("ERROR: cannot open file '%s'\n Exiting...\n", argv[1]);
+    return 1;
+  };
+
+  char calibrationString [MAX_STRING_LENGTH] = {0};
+  char firstDigit;
+  char lastDigit;
+  int calibrationNum;
+  int sum = 0;
+
+  //char stringNumbers[9] = {"one","two", "three", "four", "five", "six", "seven"}
+
+  //while not at the end of the file
+  while (!feof(inputptr)){
+    firstDigit = 0;
+    lastDigit = 0;
+    calibrationNum = 0;
+    memset(calibrationString, 0, MAX_STRING_LENGTH * sizeof(calibrationString[0]));
+    //fetch a line
+    fgets(calibrationString, MAX_STRING_LENGTH, inputptr);
+
+    for (int i = 0; i < MAX_STRING_LENGTH; i++){
+      if(isdigit(calibrationString[i])){
+        lastDigit = calibrationString[i] - '0';
+        if (firstDigit == 0){
+          firstDigit = calibrationString[i] - '0';
+        };
+      };
+    };
+    calibrationNum = firstDigit*10+lastDigit;
+    sum += calibrationNum;
+};
+  fclose(inputptr);
+  printf("the sum was: %i\n", sum);
 
 };
